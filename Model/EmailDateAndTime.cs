6f81ea8e3a08477d2e71136e23c1165df664691f
Robert <robertcorreas@gmail.com>
2014-07-11 21:05:58 -0300

    First commit - EmailConfig, EmailDateAndTime and EmailProperties classes

---
+++ Model/EmailDateAndTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Model
+{
+    public class EmailDateAndTime
+    {
+        public DateTime StartHour { get; private set; }
+
+        public DateTime EndHour { get; private set; }
+
+        public DateTime Day { get; private set; }
+
+        public EmailDateAndTime(DateTime day, DateTime startHour, DateTime endHour)
+        {
+            Day = day;
+            StartHour = startHour;
+            EndHour = endHour;
+        }
+
+        public DateTime GetStartEmailDateTime()
+        {
+            var r = new Random();
+
+            var newStartHour = Day.AddHours(StartHour.Hour);
+            var newStartMinute = Day.AddMinutes(StartHour.Minute + r.Next(-5, 5));
+
+            return Day.AddHours(newStartHour.Hour).AddMinutes(newStartMinute.Minute);
+        }
+
+        public DateTime GetEndEmailDateTime()
+        {
+            var r = new Random();
+            var newEndHour = Day.AddHours(EndHour.Hour);
+            var newEndMinute = Day.AddMinutes(EndHour.Minute + r.Next(-5, 5));
+
+            return Day.AddHours(newEndHour.Hour).AddMinutes(newEndMinute.Minute);
+        }
+    }
+}
---
+++ Model/EmailProperties.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mail;
+
+namespace Model
+{
+    public class EmailProperties
+    {
+        public EmailProperties(string senderEmail, string senderName, string receiverEmail, string receiverName,
+            string subject)
+        {
+            SenderEmail = senderEmail;
+            SenderName = senderName;
+            ReceiverEmail = receiverEmail;
+            ReceiverName = receiverName;
+            Subject = subject;
+        }
+
+        public string SenderEmail { get; private set; }
+
+        public string SenderName { get; private set; }
+
+        public string ReceiverEmail { get; private set; }
+
+        public string ReceiverName { get; private set; }
+
+        public string Subject { get; private set; }
+
+        public MailMessage GetMailMessage()
+        {
+            var sender = new MailAddress(SenderEmail, SenderName);
+            var receiver = new MailAddress(ReceiverEmail, ReceiverName);
+
+            return new MailMessage(sender, receiver) { Subject = Subject };
+        }
+    }
+}
